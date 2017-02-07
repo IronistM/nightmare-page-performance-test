e824bb5c46fbb2e7c60f19e27fa3710b6a39cd3a
ironistm <parzakonis.m@gmail.com>
2017-02-07 17:47:12 +0200

    Added comments iin bash script

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Some definitions to start with...
 filename=results_$(date +%Y%m%d)_$(date +%H%M).csv
 directory='Output'
-for counter in $(seq 1 2);
+
+# Do the runs!
+for counter in $(seq 1 2); # Needed to work on nmh 4.3 bash
 do
   echo $counter
   baseline=$(node nightmare.js 'https://www.e-food.gr/delivery/neo-hrakleio/e-fooders-caf')
   secondary=$(node nightmare.js 'https://www.e-food.gr/delivery/neo-irakleio/club-souvlaki')
+  # Redirect output to the file defined a top
   echo $baseline, $secondary >> $directory/$filename
 done
