5af6ded1494fa1a7629df2c88a37efef45094c52
ironistm <parzakonis.m@gmail.com>
2017-02-07 17:45:02 +0200

    Added directory as parameter

--- runtest.sh
+++ runtest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 filename=results_$(date +%Y%m%d)_$(date +%H%M).csv
-for counter in $(seq 1 10);
+directory='Output'
+for counter in $(seq 1 2);
 do
   echo $counter
   baseline=$(node nightmare.js 'https://www.e-food.gr/delivery/neo-hrakleio/e-fooders-caf')
   secondary=$(node nightmare.js 'https://www.e-food.gr/delivery/neo-irakleio/club-souvlaki')
-  echo $baseline, $secondary >> $filename
+  echo $baseline, $secondary >> $directory/$filename
 done
